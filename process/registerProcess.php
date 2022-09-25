abe0ce6eda70d655abe7fe47e8889e5612bf1f3e
captbay <agespramana9@gmail.com>
2022-09-25 22:41:39 +0700

    tambahkan fungsi unik untuk email dan phonenum

--- process/registerProcess.php
+++ process/registerProcess.php
@@ -11,25 +11,76 @@
         $name = $_POST['name'];
         $phonenum = $_POST['phonenum'];
         $membership = $_POST['membership'];
-        // Melakukan insert ke databse dengan query dibawah ini
-        $query = mysqli_query($con,
-        "INSERT INTO users(email, password, name, phonenum, membership)
-        VALUES
-        ('$email', '$password', '$name', '$phonenum', '$membership')")
-        or die(mysqli_error($con)); 
-        // perintah mysql yang gagal dijalankan ditangani oleh perintah “or die”
-        if($query){
+        //counter ga unik
+        $gaunik = 0;
+ 
+        //number phone check kalo ada yang ga unik
+        $query = mysqli_prepare($con, "SELECT COUNT(*) FROM users WHERE phonenum = ?");
+        mysqli_stmt_bind_param($query, 's', $phonenum);
+        mysqli_stmt_execute($query);
+        $result = mysqli_stmt_get_result($query);
+        // fetch column
+        $cekPhonenum = mysqli_fetch_row($result)[0];
+    
+
+
+        //check email unik
+            // Check email
+        $query = mysqli_prepare($con, "SELECT COUNT(*) FROM users WHERE email = ?");
+        mysqli_stmt_bind_param($query, 's', $email);
+        mysqli_stmt_execute($query);
+        $result = mysqli_stmt_get_result($query);
+        // fetch column
+        $cekEmail = mysqli_fetch_row($result)[0];
+
+        // if phone number already exist
+        if ($cekEmail > 0 && $cekPhonenum > 0) {
+            $gaunik++;
             echo
             '<script>
-            alert("Register Success");
-            window.location = "../index.php"
+            alert("Email dan nomor telpon sudah di regis!");
             </script>';
-        }else{
+        } else if ($cekPhonenum > 0) { 
+            $gaunik++;
             echo
             '<script>
-            alert("Register Failed");
+            alert("Phone num sudah di regis!");
+            </script>';
+        } else if($cekEmail > 0){
+            $gaunik++;
+            echo
+            '<script>
+            alert("Email sudah di regis!");
             </script>';
         }
+
+        if($gaunik > 0) { 
+            echo
+            '<script>
+            window.history.back()
+            </script>';
+        }else{
+            // Melakukan insert ke databse dengan query dibawah ini
+            $query = mysqli_query($con,
+            "INSERT INTO users(email, password, name, phonenum, membership)
+            VALUES
+            ('$email', '$password', '$name', '$phonenum', '$membership')")
+            or die(mysqli_error($con)); 
+            // perintah mysql yang gagal dijalankan ditangani oleh perintah “or die”
+            if($query){
+                echo
+                '<script>
+                alert("Register Success");
+                window.location = "../index.php"
+                </script>';
+            }else{
+                echo
+                '<script>
+                alert("Register Failed");
+                </script>';
+            }
+        }      
+
     }else{
         echo
         '<script>
